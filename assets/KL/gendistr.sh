579424226a02434c5f1ace61be271c0d7705ba5a
cdalgicdir <cdalgicdir@ku.edu.tr>
2014-01-23 13:08:26 +0200

    KL:gendistr.sh upd

--- assets/KL/gendistr.sh
+++ assets/KL/gendistr.sh
@@ -13,14 +13,17 @@ for i in `seq 0 $((${#bondlist[@]}-1))`; do
 done
 
 for a in ${anglist[@]}; do
-    echo ang_${a} | g_angle -f -n bonded.ndx -type angle -od ang_${a}.xvg -ov time-ang_${a}.xvg
+    echo ang_${a} | g_angle -f -n bonded.ndx -type angle -od ang_${a}.xvg -ov time-ang_${a}.xvg -all
+    g_analyze -f time-ang_${a}.xvg -dist hist_ang_${a}.xvg
 done
 
 for d in ${dihlist[@]}; do
-    echo dih_${d} | g_angle -f -n bonded.ndx -type dihedral -od dih_${d}.xvg -ov time-dih_${d}.xvg
+    echo dih_${d} | g_angle -f -n bonded.ndx -type dihedral -od dih_${d}.xvg -ov time-dih_${d}.xvg -all
+    g_analyze -f time-dih_${d}.xvg -dist hist_dih_${d}.xvg
 done
 
 for i in ${implist[@]}; do
-    echo imp_${i} | g_angle -f -n bonded.ndx -type improper -od imp_${i}.xvg -ov time-imp_${i}.xvg
+    echo imp_${i} | g_angle -f -n bonded.ndx -type improper -od imp_${i}.xvg -ov time-imp_${i}.xvg -all
+    g_analyze -f time-imp_${i}.xvg -dist hist_imp_${i}.xvg
 done
 
