21f88cac8e83a43bdcc120ad01f2680190a41aa4
cdalgicdir <cdalgicdir@ku.edu.tr>
2014-02-10 15:52:33 +0200

    assets:gendistr-gro tidied

--- assets/KL/gendistr-gro.sh
+++ assets/KL/gendistr-gro.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
+trj='$trj'
+tpr='topol.tpr'
+ndx='$ndx'
+
 bondlist=('CN-CA' 'CA-CN' 'CA-L' 'CA-KC' 'KC-KN')
 bondRlist=('0.2112' '0.196' '0.263' '0.259' '0.3194')
 bondNlist=('14' '14' '8' '6' '6')
 anglist=('CN-CA-CN' 'CA-CN-CA' 'CN-CA-L' 'L-CA-CN' 'CN-CA-KC' 'KC-CA-CN' 'CA-KC-KN')
-dihlist=('CN-CA-CN-CA' 'CA-CN-CA-CN' 'CA-CN-CA-KC' 'CN-CA-KC-KN' 'CA-CN-CA-L')
+dihlist=('CN-CA-CN-CA' 'CA-CN-CA-CN' 'CA-CN-CA-KC' 'KC-CA-CN-CA' 'CN-CA-KC-KN' 'KN-KC-CA-CN' 'CA-CN-CA-L' 'L-CA-CN-CA')
 implist=('CA-CN-CN-L' 'CA-CN-KC-CN')
+
+if [[ -f $trj ]]; then
 
 mkdir -p histograms time-data
 
@@ -13,7 +19,7 @@ for i in `seq 0 $((${#bondlist[@]}-1))`; do
     for j in `seq 1 ${bondNlist[$i]}`; do
 	b=${bondlist[$i]}
 	r=${bondRlist[$i]}
-	echo bond_${b}-$j | g_bond -f traj.xtc -s -n bonded.ndx -o bond_${b}-$j.xvg -d time_bond_${b}-$j.xvg -blen $r
+	echo bond_${b}-$j | g_bond -f $trj -s $tpr -n $ndx -o bond_${b}-$j.xvg -d time_bond_${b}-$j.xvg -blen $r
 	mv bond_${b}-$j.xvg histograms/
 	mv time_bond_${b}-$j.xvg time-data/
     done
@@ -22,13 +28,13 @@ done
 for i in `seq 0 $((${#bondlist[@]}-1))`; do
     b=${bondlist[$i]}
     r=${bondRlist[$i]}
-    echo bond_${b} | g_bond -f traj.xtc -s -n bonded.ndx -o bond_${b}_avg.xvg -d time_bond_${b}.xvg -blen $r
+    echo bond_${b} | g_bond -f $trj -s $tpr -n $ndx -o bond_${b}_avg.xvg -d time_bond_${b}.xvg -blen $r
     mv bond_${b}_avg.xvg histograms/
     mv time_bond_${b}.xvg time-data/
 done
 
 for a in ${anglist[@]}; do
-    echo ang_${a} | g_angle -f traj.xtc -n bonded.ndx -type angle -od ang_${a}_avg.xvg -ov time_ang_${a}.xvg -all
+    echo ang_${a} | g_angle -f $trj -s $tpr -n $ndx -type angle -od ang_${a}_avg.xvg -ov time_ang_${a}.xvg -all
     g_analyze -f time_ang_${a}.xvg -dist ang_${a}.xvg -bw 1
     python split-hists.py ang_${a}.xvg
     rm ang_${a}.xvg
@@ -42,7 +48,7 @@ for a in ${anglist[@]}; do
 done
 
 for d in ${dihlist[@]}; do
-    echo dih_${d} | g_angle -f traj.xtc -n bonded.ndx -type dihedral -od dih_${d}_avg.xvg -ov time_dih_${d}.xvg -all
+    echo dih_${d} | g_angle -f $trj -s $tpr -n $ndx -type dihedral -od dih_${d}_avg.xvg -ov time_dih_${d}.xvg -all
     g_analyze -f time_dih_${d}.xvg -dist dih_${d}.xvg -bw 1
     python split-hists.py dih_${d}.xvg
     rm dih_${d}.xvg
@@ -56,7 +62,7 @@ for d in ${dihlist[@]}; do
 done
 
 for i in ${implist[@]}; do
-    echo imp_${i} | g_angle -f traj.xtc -n bonded.ndx -type improper -od imp_${i}_avg.xvg -ov time_imp_${i}.xvg -all
+    echo imp_${i} | g_angle -f $trj -s $tpr -n $ndx -type improper -od imp_${i}_avg.xvg -ov time_imp_${i}.xvg -all
     g_analyze -f time_imp_${i}.xvg -dist imp_${i}.xvg -bw 1
     python split-hists.py imp_${i}.xvg
     for f in `ls imp_${i}*.xvg`; do
@@ -70,7 +76,7 @@ for i in ${implist[@]}; do
 done
 
 for i in `seq 1 11`; do
-    echo dist-CNi-CNi4-${i}a dist-CNi-CNi4-${i}b | g_dist -f traj.xtc -n bonded.ndx -o time_dist_CNi-CNi4-$i
+    echo dist-CNi-CNi4-${i}a dist-CNi-CNi4-${i}b | g_dist -f $trj -s $tpr -n $ndx -o time_dist_CNi-CNi4-$i
 done
 
 awk '{if ($1 !~ /^[@|#]/) print $2}' time_dist_CNi-CNi4-1.xvg > time_dist_CNi-CNi4.xvg
@@ -101,4 +107,8 @@ rm out.tmp out2.tmp
 rm dist_CNi-CNi4.xvg time_dist_CNi-CNi4.xvg
 mv dist_CNi-CNi4-*.xvg dist_CNi-CNi4_avg.xvg histograms/
 mv time_dist_CNi-CNi4-* time_dist_CNi-CNi4_avg.xvg time-data/
+
+else
+    echo "$trj not found!"
+fi
 
