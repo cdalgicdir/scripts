e6a08e9f7f8a5ac19585867779e24e7d767bde73
cdalgicdir <dalgicdir@cpc.tu-darmstadt.de>
2016-12-30 13:53:12 +0100

    dotfiles: install.sh added

---
+++ dotfiles/install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+DIR=$HOME/scripts/dotfiles
+
+ln -s $DIR/bash_aliases .bash_aliases
+ln -s $DIR/bash_aliases_soft .bash_aliases_soft
+ln -s $DIR/bashrc .bashrc
+ln -s $DIR/bash_logout .bash_logout
+ln -s $DIR/dircolors .dircolors
+ln -s $DIR/gnuplot .gnuplot
+ln -s $DIR/gvimrc .gvimrc
+ln -s $DIR/vimrc .vimrc
+ln -s $DIR/multitailrc .multitailrc
+ln -s $DIR/tmux.conf .tmux.conf 
+ln -s $DIR/vmdrc .vmdrc 
+ln -s $DIR/vmdrc .vmdrc 
+
